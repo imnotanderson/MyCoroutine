e178e216a66a204f7de7c79b3fe1b6f45d914a6b
J <36326068@qq.com>
2017-08-24 19:48:29 +0800

    Update README.md

--- MyCoroutine.cs
+++ MyCoroutine.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Diagnostics;
 
+
 public static class MyCoroutine
 {
     public static void Init(int maxFrameIntervalMs) {
@@ -19,26 +20,31 @@ public static class MyCoroutine
         action();
     }
 
+    public static int StartDelayActionUnscale(this GameObject go, float delayTime, Action action)
+    {
+        return go.StartContinueCoroutine(DelayAction(delayTime, action), false);
+    }
+    
     public static int StartDelayAction(this GameObject go, float delayTime, Action action)
     {
         return go.StartContinueCoroutine(DelayAction(delayTime, action));
     }
 
-    public static int StartContinueCoroutine(this GameObject go, IEnumerator ie)
+    public static int StartContinueCoroutine(this GameObject go, IEnumerator ie, bool isByScaledTime = true)
     {
-        if (GameMain.IsPlaying == false) return 0;
-        return MyCoroutineMono.instance.StartMyCoroutine(go, ie);
+        if (MyCoroutineMono.IsPlaying == false) return 0;
+        return MyCoroutineMono.instance.StartMyCoroutine(go, ie, isByScaledTime);
     }
 
     public static void StopContinueCoroutineById(this GameObject go, int id)
     {
-        if (GameMain.IsPlaying == false) return;
+        if (MyCoroutineMono.IsPlaying == false) return;
         MyCoroutineMono.instance.StopMyCoroutineById(id);
     }
 
     public static void StopContinueCoroutineByGo(this GameObject go)
     {
-        if (GameMain.IsPlaying == false) return;
+        if (MyCoroutineMono.IsPlaying == false) return;
         MyCoroutineMono.instance.StopMyCoroutineByGo(go);
     }
 
@@ -58,6 +64,14 @@ public static class MyCoroutine
 
     class MyCoroutineMono : MonoBehaviour
     {
+        private static bool isPlaying = false;
+        public static bool IsPlaying
+        {
+            get
+            {
+                return isPlaying;
+            }
+        }
         public static MyCoroutineMono instance
         {
             get
@@ -74,7 +88,7 @@ public static class MyCoroutine
         static object mLock = new object();
 
         public static void Init() {
-            if (!GameMain.IsPlaying)
+            if (!MyCoroutineMono.IsPlaying)
             {
                 return;
             }
@@ -87,7 +101,6 @@ public static class MyCoroutine
                 DontDestroyOnLoad(go);
             }
         }
-        public bool printLog = false;
         Stopwatch sw = new Stopwatch();
         int coroutineId = 0;
         int coroutineCount = 0;
@@ -99,28 +112,33 @@ public static class MyCoroutine
             public IEnumerator ie;
             public float delayTime;
             public int id;
-
+            public bool isByScaledTime;
+            
             public void Upt()
             {
-                if (go == null || go.ToString() == "null")
+                if (go == null || go.Equals("null"))
                 {
                     die = true;
                     return;
                 }
+
                 if (CheckGo() == false)
                 {
                     return;
                 }
+
                 if (delayTime >= 0)
                 {
-                    delayTime -= Time.deltaTime;
+                    delayTime -= isByScaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
                 }
                 if (delayTime >= 0) return;
-                if (ie.MoveNext() == false)
+                var moveNextResult = ie.MoveNext();
+                if (moveNextResult == false)
                 {
                     die = true;
                     return;
                 }
+
                 object obj = ie.Current;
                 if (obj == null) return;
                 if (obj is float)
@@ -167,10 +185,6 @@ public static class MyCoroutine
             {
                 if (MAX_FRAME_INTERVAL_MS>0 && sw.ElapsedMilliseconds > MAX_FRAME_INTERVAL_MS)
                 {
-                    if (printLog)
-                    {
-                        UnityEngine.Debug.Log("=========>sw.ElapsedMilliseconds > MAX_FRAME_INTERVAL_MS: loopCount:" + i + "/" + dataList.Count);
-                    }
                     break;
                 }
                 var data = dataList[i];
@@ -181,6 +195,7 @@ public static class MyCoroutine
                 data.Upt();
                 dataList[i] = data;
             }
+
             for (int i = 0; i < dataList.Count; i++)
             {
                 if (dataList[i].die)
@@ -197,13 +212,14 @@ public static class MyCoroutine
             coroutineCount = dataList.Count;
         }
 
-        public int StartMyCoroutine(GameObject go, IEnumerator ie)
+        public int StartMyCoroutine(GameObject go, IEnumerator ie,bool isByScaledTime)
         {
             int id = getId();
             Data da = new Data();
             da.ie = ie;
             da.go = go;
             da.id = id;
+            da.isByScaledTime = isByScaledTime;
             lock (dataBufferList)
             {
                 dataBufferList.Add(da);
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# MyCoroutine
-一个用于Unity的协程,gameobject.active=false再重新true依然可以继续执行
+MyCoroutine
+====
+一个更加高效,可控,安全的Unity协程.
 
-# todo
-- 模块化接口
+Features
+---------
+
+* 可在物体非激活时启动,当物体启动是自动运行协程,当物体非激活时暂停.
+* 跟随物体生命周期销毁
+* 框架0GC
+* 替换成本极低,只需替换Unity的StartCoroutine为StartContinueCoroutine即可.
+* 可扩展,支持自定义协程事件.
+
+Community
+---------
+
+* QQ 群：46914035
